0a7d9f13553f367139fe422cd93dbc1304f979fb
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-25 21:27:06 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+decimal x, y, z;
+Console.WriteLine("Digite um numero");
+x = Convert.ToDecimal(Console.ReadLine()!);
+
+Console.WriteLine("Digite um numero");
+y = Convert.ToDecimal(Console.ReadLine()!);
+
+Console.WriteLine("Digite um numero");
+z = Convert.ToDecimal(Console.ReadLine()!);
+
+if (x >= y && x >= z)
+{
+    Console.Write("O maior número entre os 3 é...: ");
+    Console.WriteLine($"{x}");
+}
+ 
+else if(y >= x && y >= z)
+{
+Console.Write("O maior número entre os 3 é...: ");
+Console.WriteLine($"{y}");
+}
+
+else if(z >= x && z >= y)
+{
+Console.Write("O maior número entre os 3 é...: ");
+Console.WriteLine($"{z}");
+
+}
+else
+{
+    Console.WriteLine("erro ");
+}
